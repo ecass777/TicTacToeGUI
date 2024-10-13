8d196705e8a98b2371e19bc54d363d1b4440e791
Elijah M. Cassidy <ecass777@gmail.com>
2024-10-13 16:13:25 -0400

    First commit

---
+++ src/TicTacToeRunner.java
@@ -0,0 +1,8 @@
+public class TicTacToeRunner {
+    public static void main(String[] args) {
+        // Initialize and display the frame
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new TicTacToeFrame();
+        });
+    }
+}
